0f571ece462b6bb0b6aa4ef772504146220a14bd
trinity-senpai <sihletrinity70@gmail.com>
2021-09-02 21:44:02 +0200

    update: move functions

--- server.js
+++ server.js
@@ -100,32 +100,3 @@ app.post('/login', (req, res) => {
         }
     })
 })
-
-
-function createToken(payload, secret, expiresIn = '1d') { // token expires in one day by default
-    const token = jwt.sign(payload, secret, {
-        algorithm: process.env.JWT_ALGO,
-        expiresIn,
-    });
-    return token;
-}
-
-function getUserByEmail(email) {
-    return db.findOne({
-        email
-    }, (err, doc) => {
-        return doc
-    })
-}
-
-function storeUser(db, data, hashedPassword) {
-    // store to database
-    db.insert({
-        created: Date.now().toString(),
-        username: data.name,
-        email: data.email,
-        password: hashedPassword
-    }, (err) => {
-        return 'error occured'
-    })
-}
