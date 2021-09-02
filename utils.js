d3e27ede377eaf6c70806f30a5885c65d2a1faea
trinity-senpai <sihletrinity70@gmail.com>
2021-09-02 21:43:46 +0200

    update: move functions

--- utils.js
+++ utils.js
@@ -1,1 +1,30 @@
 //! this file is for accessing useful function
+
+// function for creating token
+function createToken(payload, secret, expiresIn = '1d') { // token expires in one day by default
+    const token = jwt.sign(payload, secret, {
+        algorithm: process.env.JWT_ALGO,
+        expiresIn,
+    });
+    return token;
+}
+
+function getUserByEmail(email) {
+    return db.findOne({
+        email
+    }, (err, doc) => {
+        return doc
+    })
+}
+
+function storeUser(db, data, hashedPassword) {
+    // store to database
+    db.insert({
+        created: Date.now().toString(),
+        username: data.name,
+        email: data.email,
+        password: hashedPassword
+    }, (err) => {
+        return 'error occured'
+    })
+}
