d8dc8df02b28d93f6d36ff1d11c4cb0f20a60c86
trinity-senpai <sihletrinity70@gmail.com>
2021-09-03 12:11:03 +0200

    update: send get request to get protected data

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,7 +1,29 @@
-import { sendRequest } from './forms';
+sendRequest('/api/dashboard', 'GET', '', localStorage.getItem('jwtlwpToken')).then((res) => {
 
-sendRequest('/dashboard', 'GET', '', localStorage.getItem('lwpToken')).then((res) => {
-    
 }).catch((err) => {
     console.log('an error occured: ', err)
 })
+
+/**
+ * Sends request to route
+ * @param {String} route The endpoint to send request to
+ * @param {String} method The request method
+ * @param {any} data the data to attach to the body header
+ * @param {String} authToken the token to be attached on the authorization header to access secure route
+ * @returns response from server
+ */
+ async function sendRequest(route, method, data, authToken = '') {
+    const options = {
+        method: method,
+        headers : {
+            'Content-Type' : 'application/x-www-form-urlencoded'
+        },
+        body : data,
+        authorization: authToken
+    }
+   
+    let res = await fetch(route, options)
+    let json =  res.json(); 
+
+    return json;
+}
